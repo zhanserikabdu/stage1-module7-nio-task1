50998e80793e5f9993344aa25dd150ea801aca03
NANO <File changed>
2022-09-09 00:19:16 +0600

    2 submission

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        Path path = Paths.get(file.getPath());
+        try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+
+
+            String currentLine = null;
+            while ((currentLine = reader.readLine()) != null) {//while there is content on the current line
+                if (currentLine.contains("Name")) {
+                    profile.setName(currentLine.substring(currentLine.indexOf(" ") + 1, currentLine.length()));
+                }
+                if (currentLine.contains("Age")) {
+                    profile.setAge(Integer.parseInt(currentLine.substring(currentLine.indexOf(" ") + 1, currentLine.length())));
+                }
+                if (currentLine.contains("Email")) {
+                    profile.setEmail(currentLine.substring(currentLine.indexOf(" ") + 1, currentLine.length()));
+                }
+                if (currentLine.contains("Phone")) {
+                    profile.
+                            setPhone(Long.parseLong(currentLine.substring(currentLine.indexOf(" ") + 1, currentLine.length())));
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace(); //handle an exception here
+        }
+        return profile;
     }
 }
